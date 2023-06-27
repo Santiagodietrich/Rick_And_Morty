e4d19462f592a93c0f13165f0f8e101193ee28a6
santiagodietrich <santydietrich@gmail.com>
2023-06-27 16:16:40 -0300

    Primer commit

---
+++ src/components/Cards.jsx
@@ -0,0 +1,16 @@
+import Card from './Card';
+
+export default function Cards({characters}) {
+   return <div>
+      {characters.map(element =>
+         <Card
+            name={element.name}
+            status={element.status}
+            species={element.species}
+            gender={element.gender}
+            origin={element.origin.name}
+            image={element.image}
+            onClose={element.onClose}
+         />)}
+   </div>;
+}
